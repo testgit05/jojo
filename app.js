afdaf97f423fe670429f721bf627ded33fcfbbba
testinggit <patilpratik07@yahoo.com>
2024-09-28 19:38:32 +0530

    normal commit

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-//<<<<<< HEAD
-// main111 edit
-//=======
+h1 = document.querySelector('h1');
 
-//feature edit11
-//>>>>>>> feature
+function chcolor(color, delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            h1.style.color = color;
+            resolve("color changed");
+        }, delay);
+       
+    });
+}
+
+chcolor("red", 1000)
+    .then(() => {
+        return chcolor("green", 1000);
+    })
+    .then(() => {
+        chcolor("yellow", 1000);
+    });
+
